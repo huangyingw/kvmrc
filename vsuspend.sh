ea541c8ad748efcb4190c81abbcfbd1eae19d9f9
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-03 20:39:48 -0700

    add the flexibility to handle one/many vm on vresume.sh and vsuspend.sh

--- vsuspend.sh
+++ vsuspend.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-virsh list|awk '/running/{print $2}'|while read ss; do virsh suspend $ss; done
+if [ -z "$1" ];
+then
+  virsh list|awk '/running/{print $2}'|while read ss; do virsh suspend $ss; done
+else
+  virsh suspend "$1"
+fi
