142a5d64ab62ad7e7529558a9abd3deeec05444d
huangyingw@linux_programming <huangyingw@gmail.com>
2013-07-22 22:39:08 -0700

    update vreset.sh

--- vreset.sh
+++ vreset.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-TARGET=$1
-virsh destroy "$TARGET" \
-  ; virsh start "$TARGET"
+if [ -z "$1" ];
+then
+  virsh list|awk '/running/{print $2}'|while read ss; do virsh destroy "$ss" ; virsh start "$ss"; done
+else
+  TARGET=$1
+  virsh destroy "$TARGET" ; virsh start "$TARGET"
+fi
