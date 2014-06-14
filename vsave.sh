020e6c2e5ace2a1c6a3b57ae0c110e11b78b38c0
huangyingw@linux_programming <huangyingw@gmail.com>
2014-06-14 12:15:01 -0700

    n

--- vsave.sh
+++ vsave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ];
 then
-  virsh list|awk '/running/{print $2}'|while read ss; do virsh save $ss; done
+  virsh list|awk '/running/{print $2}'|while read ss; do virsh save $ss /media/volgrp/kvm/save/$ss.save; done
 else
-  virsh save "$1"
+  virsh save "$1" /media/volgrp/kvm/save/"$1".save
 fi
