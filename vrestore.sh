b53ccae59b58fbdfa615d89723917aeecb18a0b3
huangyingw@linux_programming <huangyingw@gmail.com>
2014-06-16 14:16:47 -0700

    n

--- vrestore.sh
+++ vrestore.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-find /media/volgrp/kvm/save/ -type f -maxdepth 1 -name \*.save|while read ss; do virsh restore $ss;rm $ss; done
+if [ -z "$1" ];
+then
+  find /media/volgrp/kvm/save/ -type f -maxdepth 1 -name \*.save|while read ss; do virsh restore $ss;rm $ss; done
+else
+  virsh restore /media/volgrp/kvm/save/"$1".save
+fi
--- vsave.sh
+++ vsave.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -z "$1" ];
 then
-  virsh list|awk '/running/{print $2}'|while read ss; do virsh save $ss /media/volgrp/kvm/save/$ss.save; done
+  virsh list|awk '/running/{print $2}'|while read ss; do virsh save $ss /media/volgrp/kvm/save/$ss.save --verbose; done
 else
-  virsh save "$1" /media/volgrp/kvm/save/"$1".save
+  virsh save "$1" /media/volgrp/kvm/save/"$1".save --verbose
 fi
